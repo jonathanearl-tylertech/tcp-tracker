92e80c7109d0c5e58406822271dc80b9c268bce5
Jonathan Earl <jonathan.earl@tylertech.com>
2023-06-05 21:45:56 -0700

    initial

---
+++ client/src/icons/letter.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	export let letterValue: string;
+</script>
+
+<span
+	class="flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border border-gray-700 bg-white-800 text-[0.625rem] font-medium text-gray-800 group-hover:text-white"
+>
+	{letterValue}
+</span>
---
+++ client/src/routes/+page.svelte
@@ -0,0 +1,61 @@
+<script>
+	import { onMount } from 'svelte';
+	import Github from '../icons/github.svelte';
+	import Testtube from '../icons/testtube.svelte';
+	import Letter from '../icons/letter.svelte';
+
+	const projects = [
+		{
+			project: 'tcp-profile-c',
+			github: 'https://github.com/tyler-technologies/tcp-launcher-e',
+			ci: '',
+			qa: '',
+			prod: ''
+		},
+		{
+			project: 'tcp-launcher-e',
+			github: 'https://github.com/tyler-technologies/tcp-launcher-e',
+			ci: '',
+			qa: '',
+			prod: ''
+		}
+	];
+
+	onMount(async () => {
+		const forge = await import('@tylertech/forge');
+
+		forge.defineButtonComponent();
+		forge.defineListComponent();
+		forge.defineListItemComponent();
+		forge.defineCheckboxComponent();
+	});
+</script>
+
+<forge-list>
+	{#each projects as item}
+		<forge-list-item>
+			<span slot="title">{item.project}</span>
+			<span slot="trailing" class="trailing">
+				<a href={item.github}><Github /></a>
+				<a href={item.ci}><Letter letterValue="ci" /></a>
+				<a href={item.qa}><Letter letterValue="qa" /></a>
+				<a href={item.prod}><Letter letterValue="prod" /></a>
+			</span>
+		</forge-list-item>
+	{/each}
+</forge-list>
+
+<forge-icon name="cog" external class="text-slate-700"></forge-icon>
+
+<style>
+	forge-list {
+		margin: 0 auto;
+		max-width: 700px;
+	}
+
+	.trailing {
+		display: flex;
+		gap: 12px;
+		align-items: center;
+	}
+</style>
